d0ecbc2a2043062eae9ff999ef119e2704782259
Leonardo <lhr@live.nl>
2017-10-21 14:10:46 -0200

    Intefracao com travis

---
+++ src/test/java/com/opensanca/trilharest/filmes/filmesTest/FilmeTest.java
@@ -0,0 +1,48 @@
+package com.opensanca.trilharest.filmes.filmesTest;
+
+import com.opensanca.trilharest.filmes.filmes.Filme;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class FilmeTest {
+
+    @Test
+    public void foraDeExibicaoSeDatasNulas() {
+        Filme filme = new Filme();
+        boolean emExibicao = filme.emExibicao();
+        Assert.assertFalse(emExibicao);
+    }
+
+    @Test
+    public void emExibicaoSeDentroDeIntervaloDeDatas() {
+        Filme filme = new Filme(null, null, null, null,
+                                LocalDate.of(2017, 10, 1),
+                                LocalDate.of(2017,10, 30));
+        boolean resultado = filme.emExibicao();
+        Assert.assertTrue(resultado);
+
+    }
+
+    @Test
+    public void foraDeExibicaoSeForaDeIntervaloDeDatas() {
+        Filme filme = new Filme(null, null, null, null,
+                LocalDate.of(2017, 10, 1),
+                LocalDate.of(2017,10, 30));
+        boolean resultado = filme.emExibicao();
+        Assert.assertFalse(resultado);
+
+    }
+
+    @Test
+    public void foraDeExibicaoSeFimExatamenteHoje() {
+        Filme filme = new Filme(null, null, null, null,
+                LocalDate.of(2017, 10, 1),
+                LocalDate.of(2017,10, 30));
+        boolean resultado = filme.emExibicao();
+        Assert.assertTrue(resultado);
+
+    }
+
+}
